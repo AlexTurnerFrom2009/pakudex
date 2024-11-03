90f7dadee5f711fe7b515f38a8240bdabc0cd08f
AlexTurnerFrom2009 <lwrmail2@gmail.com>
2024-11-03 22:46:22 +0000

    Added logic to files

--- pakudex.py
+++ pakudex.py
@@ -1,1 +1,19 @@
 
+class Pakudex:
+  stored_pakuri = []
+  species_list = [pakuri.species for pakuri in self.stored_pakuri]
+  def __init__(self, capacity=20):
+    self.max = capacity
+  def get_size(self):
+    return len(self.stored_pakuri)
+  def get_capacity(self):
+    return self.max
+  def get_species_array(self):
+    return self.species_list
+  def get_stats(self, species):
+    if not species in self.species_list:
+      return None
+    for pakuri in self.stored_pakuri
+
+
+  
--- pakuri_program.py
+++ pakuri_program.py
@@ -1,1 +1,4 @@
-
+if __name__ == "__main__":
+  print("Welcome to Pakudex: Tracker Extraordinaire!")
+  max_capacity = input("Enter max capacity of the Pakudex:")
+  print(f"The Pakudex can hold {max_capacity} species of Pakuri.")
