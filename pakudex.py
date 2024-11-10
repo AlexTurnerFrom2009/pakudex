c1c668fd8418cac9319c9a67d3c957f4f56fff64
AlexTurnerFrom2009 <lwrmail2@gmail.com>
2024-11-10 00:14:52 +0000

    Updated all files

--- pakudex.py
+++ pakudex.py
@@ -1,22 +1,49 @@
-
+from pakuri import Pakuri
 class Pakudex:
-  stored_pakuri = []
-  species_list = [pakuri.species for pakuri in stored_pakuri]
+
   def __init__(self, capacity=20):
     self.max = capacity
+    self.stored = {}
+    self.species_list = list(stored.keys())
+
   def get_size(self):
-    return len(self.stored_pakuri)
+    return len(self.stored)
+
   def get_capacity(self):
     return self.max
+
   def get_species_array(self):
-    return self.species_list
+    if len(self.stored) > 0:
+      return self.species_list
+    return None
+
   def get_stats(self, species):
-    for pakuri in stored_pakuri:
-      if pakuri.species == species:
-        stats = (pakuri.attack, pakuri.defense, pakuri.speed)
+    for pakuri in self.species_list:
+      if pakuri == species:
+        stats = [self.stored[pakuri].attack, self.stored[pakuri].defense, self.stored[pakuri].speed]
         return stats
       else:
         return None
+
+  def sort_pakuri(self):
+    new_species_list = sorted(self.species_list)
+    new_stored = {species: self.stored[species] for species in new_species_list}
+    self.stored = new_stored
+    self.species_list = new_species_list
+
+  def add_pakuri(self, species):
+    if species in self.species_list:
+      return False
+    self.stored.update({species: Pakuri(species)})
+    self.species_list.append(species)
+
+  def evolve_species(self, species):
+      if species in self.species_list:
+        self.stored[species].evolve()
+        return True
+      else:
+        return False
+
 
 
 
--- pakuri_program.py
+++ pakuri_program.py
@@ -1,4 +1,15 @@
+import pakuri
+from pakudex import Pakudex
+
 if __name__ == "__main__":
   print("Welcome to Pakudex: Tracker Extraordinaire!")
   max_capacity = input("Enter max capacity of the Pakudex:")
   print(f"The Pakudex can hold {max_capacity} species of Pakuri.")
+  p = Pakudex(20)
+
+  while True:
+    choice = input("\nPakudex Main Menu\n-----------------\n1. List Pakuri\n2. Show Pakuri\n3. Add Pakuri\n4. Evolve Pakuri\n5. Sort Pakuri\n6. Exit")
+    
+    if choice == 1:
+      for i, species in emurate(p.species_list):
+        print(f"{i}. {species}")
