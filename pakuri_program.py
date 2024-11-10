f3fd17ce116c934f64a7f51b900fcb400d6ae7ab
AlexTurnerFrom2009 <104808826+AlexTurnerFrom2009@users.noreply.github.com>
2024-11-09 23:06:48 -0500

    Update pakuri_program.py

--- pakuri_program.py
+++ pakuri_program.py
@@ -3,7 +3,7 @@ from pakudex import *
 if __name__ == "__main__":
   print("Welcome to Pakudex: Tracker Extraordinaire!")
   max_capacity = 0
-  while max_capacity < 0:
+  while max_capacity <= 0:
     try:
       max_capacity = int(input("Enter max capacity of the Pakudex: "))
       assert max_capacity > 0
